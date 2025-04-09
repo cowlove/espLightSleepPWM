679a073dfde9307b32900371df7b55b6c587ca82
jim@vheavy.com <jim@vheavy.com>
2025-04-09 11:47:52 -0700

    header file cleanup, introduce buttonTools.h

--- bin/test_csim.sh
+++ bin/test_csim.sh
@@ -16,26 +16,7 @@ fi
 
 if [[ $OPT == *p* ]]; then 
 echo Plotting...
-cat ./out/csim.out |  ./bin/iso_date_parser.py fanPwm Tint.v Tamb.v Tex1.v bv2 >  plane.dat
-gnuplot -e "set grid;
-f='./plane.dat'; 
-set title 'Refreshed $age minutes ago'; 
-set terminal qt size 1800,800;
-    unset y2tics ; set ytics nomirror ;
-    set key top left; 
-    set xdata time; 
-    set xtics;
-    set timefmt '%s'; 
-    set grid; 
-    set format x '%a %l:%M%p'; 
-    p [*:*][0:16]  
-    3.6 title 'Threshold'
-    ,f u 1:4 w l title 'Ambient VPD'
-    ,f u 1:3 w l title 'Cockpit Interior VPD' 
-    ,f u 1:(\$2 * 6/63 + 10) w l title 'Fan Power'
-    ;
-    pause 111;
-"
+./bin/plotcat.sh ./out/csim.out
 fi
 
 #   , f u 1:5 w l title 'Dessicant Exhaust VPD'
