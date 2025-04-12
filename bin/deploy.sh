7cd264cea118e9b9193a864825902b5663cb9e18
jim@vheavy.com <jim@vheavy.com>
2025-04-12 10:37:36 -0700

    fix broken upgrade process, wifi was disconnected before calling webUpgrade

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,6 +1,6 @@
-#!/bin/bash 
+#!/bin/bash -x 
 ssh ls 'rm simplePost/firmware.bin simplePost/firmware.ver'
-git describe --abbrev=8 --dirty --always --tags > firmware.ver
+git describe --abbrev=6 --dirty --always > firmware.ver
 scp /tmp/arduino/espLightSleepPWM/esp32c3/espLightSleepPWM.ino.bin ls:simplePost/firmware.bin
 scp firmware.ver ls:simplePost/firmware.ver
 cp /tmp/arduino/espLightSleepPWM/esp32c3/espLightSleepPWM.ino.bin ~/src/simplePost/firmware.bin
