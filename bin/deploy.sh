9fe788d5b9fe194a994b07974d08ada9e325020b
jim@vheavy.com <jim@vheavy.com>
2025-10-01 15:01:15 -0700

    bump up lipo battery

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -x 
 ssh ls 'rm simplePost/firmware.bin simplePost/firmware.ver'
 git describe --abbrev=6 --dirty --always > firmware.ver
-scp /tmp/arduino/espLightSleepPWM/esp32c3/espLightSleepPWM.ino.bin ls:simplePost/firmware.bin
+scp ./build/esp32c3/espLightSleepPWM.ino.bin ls:simplePost/firmware.bin
 scp firmware.ver ls:simplePost/firmware.ver
-cp /tmp/arduino/espLightSleepPWM/esp32c3/espLightSleepPWM.ino.bin ~/src/simplePost/firmware.bin
+cp ./build/esp32c3/espLightSleepPWM.ino.bin ~/src/simplePost/firmware.bin
 cp firmware.ver ~/src/simplePost/firmware.ver
 
     
